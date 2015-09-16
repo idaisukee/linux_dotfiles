6dc6414f1941787c9ab1696a85e0fec0cdfe92c0
Daisuke Inoue <hibariyashiki@gmail.com>
2015-09-16 14:32:29 +0900

    added shell_key_bind.zsh

---
+++ shell_key_bind.zsh
@@ -0,0 +1,6 @@
+bindkey '^B' backward-delete-char
+bindkey '^D' backward-char
+bindkey '^N' forward-char
+bindkey '^R' delete-char-or-list
+bindkey '^H' down-line-or-history
+bindkey '^T' up-line-or-history
