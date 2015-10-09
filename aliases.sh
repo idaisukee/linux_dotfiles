b505034e8b4e341ff107ee9d139d0c210ae23312
Daisuke Inoue <hibariyashiki@gmail.com>
2015-10-10 00:35:09 +0900

    copied aliases from .zshrc_old to aliases.sh.

--- aliases.sh
+++ aliases.sh
@@ -16,5 +16,30 @@ alias lo='locate'
 alias pb="rsync -r -e 'ssh -p 3843' ~/doc/weblog/ daisuke@27.120.106.39:/var/www/html/weblog"
 alias gb="rsync -r -e 'ssh -p 3843' daisuke@27.120.106.39:/home/daisuke/webroot/weblog/ ~/doc/weblog"
 alias ag='apt-get'
-alias itl='sudo apt-get install'
 alias e='echo'
+
+alias ls='ls -a'
+alias wi='wicd-cli'
+alias wg='w3m google.com'
+# alias wie='wicd-cli -yScpelms-g; ruby $HOME/elms_login.rb'
+
+alias wie0='wicd-cli -yx'
+alias wie1='ruby $HOME/connect_elms-g.rb'
+alias wie2='ruby $HOME/login_elms-g.rb'
+alias wie='wie1; sleep 15; wie2'
+
+alias n0='nmcli c up id 00xx'
+alias nh='nmcli c up id HINES-WLAN'
+
+alias nmr='sudo service network-manager restart'
+alias scan='wicd-cli -ySl'
+alias wi0='wicd-cli -yScp00xx'
+alias up='sudo apt-get update; sudo apt-get upgrade; sudo apt-get update; sudo apt-get upgrade; sudo apt-get update; sudo apt-get upgrade'
+
+alias sch='apt-cache search'
+alias schb='apt-cache -t "wheezy-backports" search'
+alias itl='sudo apt-get install'
+alias itlb='sudo apt-get -t "wheezy-backports" install'
+
+alias pow='upower -d | grep percentage'
+
