56df7493ecca5be11cc2dd86ace3a9e2dedd0ece
Daisuke Inoue <hibariyashiki@gmail.com>
2015-10-10 01:33:18 +0900

    make_tmp_aliases.sh: edited var name and comments.

--- make_tmp_aliases.sh
+++ make_tmp_aliases.sh
@@ -1,13 +1,14 @@
 #!/bin/zsh
-aliases="$HOME/linux_dotfiles/tmp_aliases.sh"
+MYaliases="$HOME/linux_dotfiles/tmp_aliases.sh"
 
-echo "alias $1='$2'" >> $aliases
-. $aliases
+echo "alias $1='$2'" >> $MYaliases
+alias $1=$2
+
 # この script は動作しない．
-# $ (this_script) AA ls
+#  this_script AA ls
 # すると，
-# - tmp_aliases.sh に `alias AA='ls'` と書き込まれる
+# - tmp_aliases.sh に alias AA='ls' と書き込まれる
 # が
-# - `$ alias` しても， AA は定義されていないし， `$ AA` も実行できない．
+# -  alias しても， AA は定義されていないし，  AA も実行できない．
 # . command の仕様を見直す必要がある．
 
