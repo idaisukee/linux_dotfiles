87516ed230eb6fda70b225a49bcb49e0891a0c07
Daisuke Inoue <hibariyashiki@gmail.com>
2015-10-16 23:25:06 +0900

    temp aliases around sleep.

--- tmp_aliases.sh
+++ tmp_aliases.sh
@@ -1,3 +1,7 @@
 alias mta='. /home/daisuke/linux_dotfiles/make_tmp_aliases.sh '
-alias AA='ls'
-alias BB='ls'
+alias val='ruby /home/daisuke/src/sleep/validate.rb '
+alias val2='ruby /home/daisuke/src/sleep/validate_2.rb '
+alias sls='ruby /home/daisuke/src/sleep/translate_sleep_time_2.rb '
+alias slm='ruby /home/daisuke/src/sleep/translate_meal_time.rb '
+alias slk='ruby /home/daisuke/src/sleep/abstract_kibun.rb '
+
