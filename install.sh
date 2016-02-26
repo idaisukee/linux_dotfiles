5dc9c5ec117a1bffd94063a37c05ca8b4416fff3
Daisuke Inoue <hibariyashiki@gmail.com>
2016-02-26 13:03:34 +0900

    started to use $HOME in install.sh.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 
-home_dir='/home/daisuke'
-dotfiles_dir='/home/daisuke/linux_dotfiles'
+home_dir=$HOME
+dotfiles_dir=$home_dir/linux_dotfiles'
 
 for files in `find $dotfiles_dir -name '.*' -printf '%f\n'` ; do
     ln -sf $dotfiles_dir/$files $home_dir/$files
