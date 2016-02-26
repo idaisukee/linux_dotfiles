21ddb24bc8bb651dfd033bbc103f4e61ec015bc6
Daisuke Inoue <hibariyashiki@gmail.com>
2016-02-26 13:05:33 +0900

    erased needless quot.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 
 home_dir=$HOME
-dotfiles_dir=$home_dir/linux_dotfiles'
+dotfiles_dir=$home_dir/linux_dotfiles
 
 for files in `find $dotfiles_dir -name '.*' -printf '%f\n'` ; do
     ln -sf $dotfiles_dir/$files $home_dir/$files
