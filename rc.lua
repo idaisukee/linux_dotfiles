bf3457325ca7c8be2551e4c4dd6d10f20d15b2e2
Daisuke Inoue <hibariyashiki@gmail.com>
2016-02-27 17:58:37 +0900

    made layout.suit.max default.

--- rc.lua
+++ rc.lua
@@ -56,6 +56,7 @@ modkey = "Mod4"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 layouts =
 {
+    awful.layout.suit.max,
     awful.layout.suit.floating,
     awful.layout.suit.tile,
     awful.layout.suit.tile.left,
@@ -65,7 +66,6 @@ layouts =
     awful.layout.suit.fair.horizontal,
     awful.layout.suit.spiral,
     awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max,
     awful.layout.suit.max.fullscreen,
     awful.layout.suit.magnifier
 }
