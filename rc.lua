79032aa5c89f76f909a94061506b0e220a783d09
Daisuke Inoue <hibariyashiki@gmail.com>
2016-02-27 16:56:55 +0900

    execute_command in rc.lua .

--- rc.lua
+++ rc.lua
@@ -99,6 +99,19 @@ mylauncher = awful.widget.launcher({ image = image(beautiful.awesome_icon),
                                      menu = mymainmenu })
 -- }}}
 
+
+-- Execute command and return its output. You probably won't only execute commands with one
+-- line of output
+-- derived from https://awesome.naquadah.org/wiki/Awesome_3_configuration
+function execute_command(command)
+   local fh = io.popen(command)
+   local str = ""
+   for i in fh:lines() do
+      str = str .. i
+   end
+   io.close(fh)
+   return str
+end
 -- {{{ Wibox
 -- Create a textclock widget
 mytextclock = awful.widget.textclock({ align = "right" })
