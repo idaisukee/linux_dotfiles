994d99c83902f79b970191625da4a06d9c9537f4
Daisuke Inoue <hibariyashiki@gmail.com>
2016-07-09 17:59:32 +0900

    gb, pb.

--- aliases.sh
+++ aliases.sh
@@ -23,9 +23,12 @@ alias m='mv'
 alias r='rm'
 alias p='cp'
 alias lo='locate'
-# alias pb="rsync -r -e 'ssh -p 3843' ~/doc/weblog/ daisuke@27.120.106.39:/var/www/html/weblog"
-alias pb="rsync -r -e 'ssh -p 3843' ~/doc/webroot/weblog/ daisuke@27.120.106.39:/home/daisuke/doc/webroot/weblog"
-alias gb="rsync -r -e 'ssh -p 3843' daisuke@27.120.106.39:/home/daisuke/webroot/weblog/ ~/doc/webroot/weblog"
+
+BLOG_LOCAL=$HOME/doc/weblog/single/
+BLOG_SRV=/home/daisuke/doc/webroot/weblog/single/
+alias pb="rsync -av -e 'ssh -p 3843' $BLOG_LOCAL daisuke@27.120.106.39:$BLOG_SRV"
+alias gb="rsync -av -e 'ssh -p 3843' daisuke@27.120.106.39:$BLOG_SRV $BLOG_LOCAL"
+#alias gb="rsync -r -e 'ssh -p 3843' daisuke@27.120.106.39:/home/daisuke/webroot/weblog/ ~/doc/webroot/weblog"
 alias ag='apt-get'
 alias e='echo'
 alias date6='date +%y%m%d'
