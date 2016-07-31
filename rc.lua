00ca5581b8952674069431f607a6cb6943621d7c
Daisuke Inoue <hibariyashiki@gmail.com>
2016-07-31 20:01:36 +0900

    awesome: mytextclock.

--- rc.lua
+++ rc.lua
@@ -88,7 +88,10 @@ mylauncher = awful.widget.launcher({ image = image(beautiful.awesome_icon),
 
 -- {{{ Wibox
 -- Create a textclock widget
-mytextclock = awful.widget.textclock({ align = "right" })
+mytextclock = awful.widget.textclock(
+   { align = "right" },
+   " %Y.%m.%d %H:%M ",
+   60)
 
 -- Create a systray
 mysystray = widget({ type = "systray" })
