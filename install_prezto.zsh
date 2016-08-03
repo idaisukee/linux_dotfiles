4c29e4335491c8e05cc9a8f162e87da7c26a4a80
Daisuke Inoue <hibariyashiki@gmail.com>
2016-08-03 21:18:30 +0900

    install_prezto

--- install_prezto.zsh
+++ install_prezto.zsh
@@ -1,3 +1,5 @@
+DOTFILES_DIR=$HOME/linux_dotfiles
+
 ln -s $HOME/.zplug/repos/sorin-ionescu/prezto $HOME/.zprezto
 mkdir oldzshdot
 cp .zshrc .zlogin .zlogout .zprofile .zshenv oldzshdot
@@ -6,3 +8,6 @@ setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
 	ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
+
+rm $HOME/.zshrc
+ln -s $DOTFILES_DIR/.zshrc $HOME/.zshrc
