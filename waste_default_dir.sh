288cd2d1876f310f5ff19a411f4905e21dea3d98
Daisuke Inoue <hibariyashiki@gmail.com>
2016-08-07 16:18:18 +0900

    modified waset_default_dr.sh

--- waste_default_dir.sh
+++ waste_default_dir.sh
@@ -1,8 +1,8 @@
 mv $HOME/ダウンロード $HOME/Downloads
+mv $HOME/デスクトップ $HOME/Desktop
 rmdir $HOME/ミュージック
 rmdir $HOME/ピクチャ
 rmdir $HOME/ビデオ
 rmdir $HOME/ドキュメント
-rmdir $HOME/デスクトップ
 rmdir $HOME/テンプレート
 rmdir $HOME/公開
