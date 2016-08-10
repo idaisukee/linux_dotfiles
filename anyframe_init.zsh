b55c411ebc62addce685cc5f49f149b49b09291f
Daisuke Inoue <hibariyashiki@gmail.com>
2016-08-10 18:25:23 +0900

    bye peco-select-history, hello anyframe-widget-execute-history.

--- anyframe_init.zsh
+++ anyframe_init.zsh
@@ -4,3 +4,4 @@ fpath=($HOME/.zplug/repos/mollifier/anyframe(N-/) $fpath)
 autoload -Uz anyframe-init
 anyframe-init
 
+bindkey '^F' anyframe-widget-execute-history
--- misc.zsh
+++ misc.zsh
@@ -14,22 +14,3 @@ setopt inc_append_history
 setopt share_history
 
 zstyle ':completion:*:default' menu select=1
-
-
-function peco-select-history() {
-    local tac
-    if which tac > /dev/null; then
-        tac="tac"
-    else
-        tac="tail -r"
-    fi
-    BUFFER=$(\history -n 1 | \
-        eval $tac | \
-        peco --query "$LBUFFER")
-    CURSOR=$#BUFFER
-    zle clear-screen
-}
-
-
-zle -N peco-select-history
-bindkey '^F' peco-select-history
