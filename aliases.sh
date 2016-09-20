f5382e8e003581c7bf5041cb4da03f43ee86b416
Daisuke Inoue <hibariyashiki@gmail.com>
2016-09-20 10:19:47 +0900

    deleted ag -> apt-get.

--- aliases.sh
+++ aliases.sh
@@ -32,7 +32,6 @@ BLOG_SRV=/home/daisuke/doc/webroot/weblog/single/
 alias pb="rsync -av -e 'ssh -p 3843' $BLOG_LOCAL daisuke@27.120.106.39:$BLOG_SRV"
 alias gb="rsync -av -e 'ssh -p 3843' daisuke@27.120.106.39:$BLOG_SRV $BLOG_LOCAL"
 #alias gb="rsync -r -e 'ssh -p 3843' daisuke@27.120.106.39:/home/daisuke/webroot/weblog/ ~/doc/webroot/weblog"
-alias ag='apt-get'
 alias e='echo'
 alias date6='date +%y%m%d'
 
