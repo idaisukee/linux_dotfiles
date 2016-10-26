3c72408e4c265a54908aec3004a166e2f0206c6c
Daisuke Inoue <hibariyashiki@gmail.com>
2016-10-26 16:04:24 +0900

    peco installers.

---
+++ installers/install_peco_64.sh
@@ -0,0 +1,3 @@
+curl -L https://github.com/peco/peco/releases/download/v0.4.4/peco_linux_amd64.tar.gz -o /tmp/peco.tar.gz
+tar xzvf /tmp/peco.tar.gz -C /tmp
+sudo cp /tmp/peco_linux_amd64/peco /usr/local/bin
