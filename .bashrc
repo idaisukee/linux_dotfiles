a07ea4003f4f2d6c0bcd6c280e58c0d9a6541343
Daisuke Inoue <hibariyashiki@gmail.com>
2016-10-26 21:16:02 +0900

    rbenv

---
+++ .bashrc
@@ -0,0 +1,1 @@
+source $HOME/linux_dotfiles/rbenv.sh
--- rbenv.sh
+++ rbenv.sh
@@ -1,17 +1,2 @@
-export PATH="/home/daisuke/.rbenv/shims:${PATH}"
-source "/usr/lib/rbenv/libexec/../completions/rbenv.zsh"
-rbenv rehash 2>/dev/null
-rbenv() {
-  typeset command
-  command="$1"
-  if [ "$#" -gt 0 ]; then
-    shift
-  fi
-
-  case "$command" in
-  rehash|shell)
-    eval `rbenv "sh-$command" "$@"`;;
-  *)
-    command rbenv "$command" "$@";;
-  esac
-}
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
