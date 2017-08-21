3a8f5bfd56d4e256fca58a293c52dac2228f1437
Daisuke Inoue <hibariyashiki@gmail.com>
2017-08-21 21:49:53 +0900

    installer

---
+++ installers/install_php5.sh
@@ -0,0 +1,1 @@
+sudo apt-get install php5 php5-cli php5-curl
---
+++ installers/install_php_libs.sh
@@ -0,0 +1,19 @@
+# install libs for php-build
+
+sudo apt-get install \
+libxml2 \
+libxml2-dev \
+openssl \
+libssl-dev \
+libsslcommon2-dev \
+bzip2 \
+libbz2-dev \
+libcurl3 \
+libcurl4-gnutls-dev \
+libjpeg-dev \
+libpng16-dev \
+libreadline-dev \
+libmcrypt-dev \
+libtidy-dev \
+libxslt1-dev \
+autoconf
