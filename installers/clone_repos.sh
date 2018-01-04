f957be1e31877e349dc5473773eb5494698ea821
Daisuke Inoue <hibariyashiki@gmail.com>
2018-01-04 15:23:46 +0900

    some

---
+++ installers/clone_repos.sh
@@ -0,0 +1,15 @@
+for r in \
+	
+Sinsituwoka.php \ 
+Yunoki.rb \ 
+hexoblog \ 
+Kensoukahuuntoutiyau \ 
+Kenrituhitiyutukuwanmahe \ 
+Nakanuma.php \ 
+Otohatiyau.rb
+
+do	
+
+git clone git@github.com:idaisukee/"$r" ~/prd/src/"$r"
+
+done
---
+++ installers/link_rc_lua.sh
@@ -0,0 +1,5 @@
+DOTDIR=$HOME/linux_dotfiles
+
+mkdir $HOME/.config/awesome
+ln -s $DOTDIR/356 $HOME/.config/awesome/rc.lua
+
