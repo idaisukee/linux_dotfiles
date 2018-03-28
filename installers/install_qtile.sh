8873d29dc476f3fc224da5d117de432d0c0ea950
Daisuke Inoue <hibariyashiki@gmail.com>
2018-03-28 17:54:10 +0900

    install_qtile.sh

---
+++ installers/install_qtile.sh
@@ -0,0 +1,5 @@
+# cf. http://blog.asw.io/howto/2016/07/31/installing-qtile-on-ubuntu
+
+sudo apt-get install python3-pip python3-xcffib python3-cairocffi libcairo2 libxcb-render0-dev libffi-dev libpangocairo-1.0-0 python-dbus
+sudo pip3 install qtile
+
