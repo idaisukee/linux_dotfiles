92a7b82f21f7bd5292b4e00fde381fd7f26d02b6
Daisuke Inoue <hibariyashiki@gmail.com>
2018-03-30 20:55:40 +0900

    qtile

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -7,6 +7,8 @@ except ImportError:
 
 from libqtile.manager import Click, Drag, Screen
 
+import os
+
 sup = "mod4"
 keys = [
     Key([sup, "shift"], "e", lazy.spawn("emacs")),
@@ -22,19 +24,32 @@ keys = [
 
 ]
 
+def query():
+    result = os.popen("ruby /home/ieremius/prd/src/rdatetime/display_5.rb").read()
+    return result
+
+# from http://www.color-hex.com/color-palette/19117
+
+deep_blue = '69d2e7'
+light_blue = 'a7dbd8'
+middle = 'e0e4cc'
+light_orange = 'f38630'
+deep_orange = 'fa6901'
+
 screens = [
     Screen(
         top = bar.Bar(
             [
-                widget.GroupBox(fontsize=10),
                 widget.Prompt(),
-                widget.WindowName(),
-                widget.Battery(
-                    font='Consolas',fontsize=12, margin_x=6),
-                widget.Sep(),
-                widget.CPUGraph(),
-                widget.MemoryGraph(),
+                widget.TaskList(highlight_method='border', borderwidth=1, border=deep_orange, rounded=False),
+                widget.CPUGraph(width=40, graph_color=light_blue, border_color='000000'),
+                widget.MemoryGraph(width=40, graph_color=deep_blue, border_color='000000'),
+                widget.Clipboard(width=100),
                 widget.Systray(),
+                widget.Battery(
+                    font='Consolas',fontsize=12, margin_x=20),
+                widget.Sep(),
+                widget.GenPollText(func=query, update_interval=0.8),
                 widget.Clock(format='%Y-%m-%dT%H:%M:%S+09:00', fontsize=12),
             ],
             25,
