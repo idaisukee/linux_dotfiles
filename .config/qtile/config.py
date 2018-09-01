41878391901f9b1b3b386e4db4b1cfff3128a88c
Daisuke Inoue <hibariyashiki@gmail.com>
2018-09-01 22:16:32 +0900

    qtile

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -69,3 +69,22 @@ screens = [
         ),
     ),
 ]
+
+groups = [
+    Group("1"),
+    Group("2"),
+    Group("3"),
+    Group("4"),
+    Group("5"),
+    Group("6"),
+    Group("7"),
+    Group("8"),
+    Group("9"),
+]
+for i in groups:
+    keys.append(
+        Key([sup], i.name, lazy.group[i.name].toscreen())
+    )
+    keys.append(
+        Key([sup, "shift"], i.name, lazy.window.togroup(i.name))
+    )
