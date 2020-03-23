118b2952d3b90f2c276dd4e5c895f1b65f5fed32
Daisuke Inoue <hibariyashiki@gmail.com>
2020-03-23 22:11:34 +0900

    map

---
+++ map_hhkb.sh
@@ -0,0 +1,3 @@
+xmodmap $MAPDIR/central.map
+xmodmap $MAPDIR/hhkb_peripheral.map
+xmodmap $MAPDIR/hhkb_switch.map
---
+++ map_x40.sh
@@ -0,0 +1,4 @@
+xmodmap $MAPDIR/central.map
+xmodmap $MAPDIR/x40_peripheral.map
+xmodmap $MAPDIR/x40_modifier.map
+xmodmap $MAPDIR/x40_switch.map
