9e05ca1468c069116ab6cce910ca7f269bdcb9d9
Ieremius <hibariyashiki@gmail.com>
2025-04-19 12:07:07 +0900

    deleted old aliases for wifi.

--- aliases.sh
+++ aliases.sh
@@ -37,20 +37,7 @@ alias md='mkdir '
 alias ls='ls -a'
 alias wi='wicd-cli'
 alias wg='w3m google.com'
-# alias wie='wicd-cli -yScpelms-g; ruby $HOME/elms_login.rb'
 
-alias wie0='wicd-cli -yx'
-alias wie1='ruby $HOME/connect_elms-g.rb'
-alias wie2='ruby $HOME/login_elms-g.rb'
-alias wie='wie1; sleep 15; wie2'
-
-alias n0='nmcli c up id 00xx'
-alias nh='nmcli c up id HINES-WLAN'
-
-alias nmr='sudo service network-manager restart'
-alias N='nmr'
-alias scan='wicd-cli -ySl'
-alias wi0='wicd-cli -yScp00xx'
 alias up='sudo apt-get update; sudo apt-get upgrade; sudo apt-get update; sudo apt-get upgrade; sudo apt-get update; sudo apt-get upgrade'
 
 alias sch='apt-cache search'
